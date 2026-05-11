099547cf9c899c05c1423d26f28a385fba98a051
Fawzee <badfab4real@gmail.com>
2026-05-11 01:47:22 +0100

    removed apply to fix bugs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,3 +22,9 @@ output "ecr_repository_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.vprofileapp.repository_url
 }
+
+# Output the VPC ID
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
